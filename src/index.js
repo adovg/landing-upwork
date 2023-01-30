4d5bd690060bca8a3aae31d4926071c8c931a41f
adovg <oleksiidovgalenko@gmail.com>
2023-01-30 23:34:46 +0200

    hero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 // import "./styles.scss";
-console.log('reload')
 
+
+window.onload = () => {
+    console.log('load')
+    console.log(window.innerWidth)
+    let burgerBtn = document.querySelector('.burger__btn');
+    //console.log(burgerBtn)
+    burgerBtn.onclick = function () {
+        console.log('click')
+        //burgerBtn.toggleAttribute('hidden')
+        hideMenu();
+    }
+
+
+    function hideMenu() {
+        const menuMobile = document.querySelector('.menu-mobile');
+        //menuMobile.toggleAttribute('hidden');
+        menuMobile.classList.toggle('hidden');
+      
+       
+    }
+
+    // if(window.innerWidth >= 768) {
+    //     menuMobile.classList.toggle = '.hidden' ;
+    // }
+
+
+
+}
